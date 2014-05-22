d2500c2ca2875605b4dd08f8be5c0243ff594231
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-22 09:30:25 +0200

    Modify user add test

--- scenarios/AddUserTest.php
+++ scenarios/AddUserTest.php
@@ -2,47 +2,46 @@
 
 use GuzzleHttp\Stream;
 
-class TestAddUser extends PHPUnit_Framework_TestCase
+class AddUserTest extends PHPUnit_Framework_TestCase
 {
     // ...
 
   public function testUserCreated()
   {
-    
     $client = new GuzzleHttp\Client();
+    $client->delete('http://localhost:8080/server.php/users/tsow',
+                    ['exceptions' => false]);
     $res = $client->put('http://localhost:8080/server.php/users/tsow', 
                         ['headers' => ['Content-Type' => 'application/json'],
                          'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow"}'//,
                          //'debug' => true
                         ]);
     $this->assertEquals(201, $res->getStatusCode()); 
-
     $res = $client->get('http://localhost:8080/server.php/users/tsow', ['headers' => ['Accept' => 'application/json']]);
-
     $data = json_decode($res->getBody(),TRUE);
-
     $this->assertEquals(200, $res->getStatusCode()); 
     $this->assertEquals("Tata", $data["first_name"]); 
-    
   }
 
   public function testUserUpdating()
-  {
-    
+  {  
     $client = new GuzzleHttp\Client();
+    $client->delete('http://localhost:8080/server.php/users/tsow',
+                    ['exceptions' => false]);
+    $client->put('http://localhost:8080/server.php/users/tsow', 
+                 ['headers' => ['Content-Type' => 'application/json'],
+                  'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow"}',
+                  'exceptions' => false]);
     $res = $client->put('http://localhost:8080/server.php/users/tsow', 
                         ['headers' => ['Content-Type' => 'application/json'],
                          'body' => '{"first_name" : "Titi","last_name" : "Sow", "login" :"tsow"}'
                         ]);
     $this->assertEquals(200, $res->getStatusCode()); 
-
-    $res = $client->get('http://localhost:8080/server.php/users/tsow', ['headers' => ['Accept' => 'application/json']]);
-
+    $res = $client->get('http://localhost:8080/server.php/users/tsow', 
+                        ['headers' => ['Accept' => 'application/json']]);
     $data = json_decode($res->getBody(),TRUE);
-
     $this->assertEquals(200, $res->getStatusCode()); 
-    $this->assertEquals("Titi", $data["first_name"]); 
-    
+    $this->assertEquals("Titi", $data["first_name"]);  
   }
 
 }
