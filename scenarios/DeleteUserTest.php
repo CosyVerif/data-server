6fe6f761ea9f87f465532dcf5d380730886962a0
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-24 14:17:17 +0200

    improvement of the delete user test

--- scenarios/DeleteUserTest.php
+++ scenarios/DeleteUserTest.php
@@ -1,37 +1,66 @@
 <?php
 
 use GuzzleHttp\Stream;
+require_once 'Constants.php';
 
 class DeleteUserTest extends PHPUnit_Framework_TestCase
 {
-    // ...
+
+  public function testUp(){
+    $this->rrmdir("resources/users");
+    $info = array('first_name' =>'Idrissa',
+                  'last_name' => 'Sokhona');
+    $auth = array('login' => 'isokhona',
+                  'password' => password_hash('isokhonatoto', PASSWORD_DEFAULT));
+    mkdir("resources/users/isokhona");
+    file_put_contents("resources/users/isokhona/info.json", json_encode($info));
+    file_put_contents("resources/users/isokhona/auth.json", json_encode($auth));
+  }
 
   public function testDeleteExistUser()
   {
     $client = new GuzzleHttp\Client();
+    $encoded = base64_encode("isokhona:toto");
     $res = $client->put('http://localhost:8080/server.php/users/udelete', 
-                        ['headers' => ['Content-Type' => 'application/json'],
-                         'body' => '{"first_name" : "User","last_name" : "Delete", "login" :"udelete"}'
+                        ['headers' => ['Content-Type' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
+                         'body' => '{"first_name" : "User","last_name" : "Delete", 
+                                     "login" :"udelete","password" : "toto"}'
                         ]);
-    $this->assertEquals(201, $res->getStatusCode()); 
-    $res = $client->get('http://localhost:8080/server.php/users/udelete', ['headers' => ['Accept' => 'application/json']]);
-    $this->assertEquals(200, $res->getStatusCode()); 
-    $res = $client->delete('http://localhost:8080/server.php/users/udelete');
-    $this->assertEquals(204, $res->getStatusCode()); 
+    $this->assertEquals(STATUS_CREATED, $res->getStatusCode()); 
     $res = $client->get('http://localhost:8080/server.php/users/udelete', 
-                        ['headers' => ['Accept' => 'application/json'],
+                        ['headers' => ['Accept' => 'application/json',
+                                       'Authorization' => 'Basic '.$encoded.'==']]);
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
+    $res = $client->delete('http://localhost:8080/server.php/users/udelete',
+                           ['headers' => ['Authorization' => 'Basic '.$encoded.'==']]);
+    $this->assertEquals(STATUS_NO_CONTENT, $res->getStatusCode()); 
+    $res = $client->get('http://localhost:8080/server.php/users/udelete', 
+                        ['headers' => ['Accept' => 'application/json',
+                                       'Authorization' => 'Basic '.$encoded.'=='],
                          'exceptions' => false
                         ]);
-    $this->assertEquals(410, $res->getStatusCode());  
+    $this->assertEquals(STATUS_GONE, $res->getStatusCode());  
   }
 
   public function testDeleteUserNotExist()
   {   
     $client = new GuzzleHttp\Client();
-    $client->delete('http://localhost:8080/server.php/users/udelete',
-                    ['exceptions' => false]);
+    $encoded = base64_encode("isokhona:toto");
     $res = $client->delete('http://localhost:8080/server.php/users/udelete',
-                           ['exceptions' => false]);
-    $this->assertEquals(404, $res->getStatusCode()); 
+                           ['headers' => ['Authorization' => 'Basic '.$encoded.'=='],
+                            'exceptions' => false]);
+    $this->assertEquals(STATUS_NOT_FOUND, $res->getStatusCode()); 
+  }
+
+  private function rrmdir($dir){
+    foreach(glob($dir . '/*') as $file) {
+        if(is_dir($file))
+            $this->rrmdir($file);
+        else
+            unlink($file);
+    }
+    if($dir != "resources/users")
+      rmdir($dir);
   }
 }
