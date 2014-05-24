e81b52bc1df46a21fbf85176dd14e049001848ad
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-24 14:18:26 +0200

    improvement of the get user test

--- scenarios/GetUserTest.php
+++ scenarios/GetUserTest.php
@@ -1,74 +1,75 @@
 <?php
 
 use GuzzleHttp\Stream;
+require_once 'Constants.php';
 
 class GetUserTest extends PHPUnit_Framework_TestCase
 {
-    // ...
+
+  public function testUp(){
+    $this->rrmdir("resources/users");
+    $info = array('first_name' =>'Idrissa',
+                  'last_name' => 'Sokhona');
+    $auth = array('login' => 'isokhona',
+                  'password' => password_hash('isokhonatoto', PASSWORD_DEFAULT));
+    mkdir("resources/users/isokhona");
+    file_put_contents("resources/users/isokhona/info.json", json_encode($info));
+    file_put_contents("resources/users/isokhona/auth.json", json_encode($auth));
+  }
 
   public function testUserFound()
   {
     $client = new GuzzleHttp\Client();
-    $client->delete('http://localhost:8080/server.php/users/isokhona',
-                    ['exceptions' => false]);
-    $client->put('http://localhost:8080/server.php/users/isokhona', 
-                 ['headers' => ['Content-Type' => 'application/json'],
-                  'body' => '{"first_name" : "Idrissa","last_name" : "Sokhona", "login" :"isokhona"}'
-                 ]);
-    $res = $client->get('http://localhost:8080/server.php/users/isokhona', ['headers' => ['Accept' => 'application/json']]);
+    $encoded = base64_encode("isokhona:toto");
+    $res = $client->get('http://localhost:8080/server.php/users/isokhona', 
+                        ['headers' => ['Accept' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'==']]);
     $data = json_decode($res->getBody(),TRUE);
-    $this->assertEquals(200, $res->getStatusCode()); 
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
     $this->assertEquals("Idrissa", $data["first_name"]);   
   }
 
   public function testUserNotFound()
   {
     $client = new GuzzleHttp\Client();
-    $client->delete('http://localhost:8080/server.php/users/other',
-                    ['exceptions' => false]);
+    $encoded = base64_encode("isokhona:toto");
     $res = $client->get('http://localhost:8080/server.php/users/other',
-                        ['headers' => ['Accept' => 'application/json'],
+                        ['headers' => ['Accept' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
                          'exceptions' => false
                         ]);
-    $this->assertEquals(404, $res->getStatusCode());   
+    $this->assertEquals(STATUS_NOT_FOUND, $res->getStatusCode());   
   }
 
   public function testUserDeleted()
   {
     $client = new GuzzleHttp\Client();
+    $encoded = base64_encode("isokhona:toto");
     $client->put('http://localhost:8080/server.php/users/toto', 
-                 ['headers' => ['Content-Type' => 'application/json'],
-                  'body' => '{"first_name" : "Tata","last_name" : "Oto", "login" :"toto"}'
-                 ]);
-    $res = $client->delete('http://localhost:8080/server.php/users/toto');
+                 ['headers' => ['Content-Type' => 'application/json',
+                                'Authorization' => 'Basic '.$encoded.'=='],
+                  'body' => '{"first_name" : "Tata","last_name" : "Oto", 
+                              "login" :"toto","password" : "toto"}']);
+    $res = $client->delete('http://localhost:8080/server.php/users/toto',
+                           ['headers' =>['Authorization' => 'Basic '.$encoded.'==']]);
     $res = $client->get('http://localhost:8080/server.php/users/toto',
-                        ['headers' => ['Accept' => 'application/json'],
+                        ['headers' => ['Accept' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
                          'exceptions' => false
                         ]);
-    $this->assertEquals(410, $res->getStatusCode());   
-  }
-  /*
-  public function testUnsupportedMediaType()
-  {
-    
-    $client = new GuzzleHttp\Client();
-    $res = $client->get('http://localhost:8080/server.php/users/other',['Accept' => 'text/html']);
-
-    //$this->assertEquals(415, $res->getStatusCode());   
-
+    $this->assertEquals(STATUS_GONE, $res->getStatusCode());   
   }
 
+  private function rrmdir($dir){
+    foreach(glob($dir . '/*') as $file) {
+        if(is_dir($file))
+            $this->rrmdir($file);
+        else
+            unlink($file);
+    }
 
-  
-  public function testIncorrectQuerySyntaxe()
-  {
-    
-    $client = new GuzzleHttp\Client();
-    $res = $client->get('http://localhost:8080/server.php/users/other');
-
-    $this->assertEquals(404, $res->getStatusCode());   
-
+    if($dir != "resources/users")
+      rmdir($dir);
   }
-  */
 }
 
