f8b93be85ef7d0647f27cdb36c9e2d116c27a803
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-24 14:37:59 +0200

    adding the authentification middleware into server

--- server.php
+++ server.php
@@ -3,7 +3,9 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
+$app->add(new \CosyVerif\Server\Core());
+$app->add(new \CosyVerif\Server\HttpBasicAuthentification());
 
-new \CosyVerif\Server\Core($app);
+
 
 $app->run();
