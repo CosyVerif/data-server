d436b692af733bc850d3c6346ef61548b12ab85e
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-24 14:15:34 +0200

    improvement the add user test

--- scenarios/AddUserTest.php
+++ scenarios/AddUserTest.php
@@ -1,47 +1,71 @@
 <?php
-
+require_once 'Constants.php';
 use GuzzleHttp\Stream;
 
 class AddUserTest extends PHPUnit_Framework_TestCase
 {
-    // ...
+
+  public function testUp(){
+    $this->rrmdir("resources/users");
+    $info = array('first_name' =>'Idrissa',
+                  'last_name' => 'Sokhona');
+    $auth = array('login' => 'isokhona',
+                  'password' => password_hash('isokhonatoto', PASSWORD_DEFAULT));
+    mkdir("resources/users/isokhona");
+    file_put_contents("resources/users/isokhona/info.json", json_encode($info));
+    file_put_contents("resources/users/isokhona/auth.json", json_encode($auth));
+  }
 
   public function testUserCreated()
   {
     $client = new GuzzleHttp\Client();
-    $client->delete('http://localhost:8080/server.php/users/tsow',
-                    ['exceptions' => false]);
+    $encoded = base64_encode("isokhona:toto");
     $res = $client->put('http://localhost:8080/server.php/users/tsow', 
-                        ['headers' => ['Content-Type' => 'application/json'],
-                         'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow"}'//,
+                        ['headers' => ['Content-Type' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
+                         'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow","password" : "toto"}'//,
                          //'debug' => true
                         ]);
-    $this->assertEquals(201, $res->getStatusCode()); 
-    $res = $client->get('http://localhost:8080/server.php/users/tsow', ['headers' => ['Accept' => 'application/json']]);
+    $this->assertEquals(STATUS_CREATED, $res->getStatusCode()); 
+    $res = $client->get('http://localhost:8080/server.php/users/tsow', 
+                        ['headers' => ['Accept' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'==']]);
     $data = json_decode($res->getBody(),TRUE);
-    $this->assertEquals(200, $res->getStatusCode()); 
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
     $this->assertEquals("Tata", $data["first_name"]); 
   }
 
   public function testUserUpdating()
   {  
     $client = new GuzzleHttp\Client();
-    $client->delete('http://localhost:8080/server.php/users/tsow',
-                    ['exceptions' => false]);
+    $encoded = base64_encode("isokhona:toto");
     $client->put('http://localhost:8080/server.php/users/tsow', 
-                 ['headers' => ['Content-Type' => 'application/json'],
-                  'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow"}',
+                 ['headers' => ['Content-Type' => 'application/json', 
+                                'Authorization' => 'Basic '.$encoded.'=='],
+                  'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow","password" : "toto"}',
                   'exceptions' => false]);
     $res = $client->put('http://localhost:8080/server.php/users/tsow', 
-                        ['headers' => ['Content-Type' => 'application/json'],
-                         'body' => '{"first_name" : "Titi","last_name" : "Sow", "login" :"tsow"}'
+                        ['headers' => ['Content-Type' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
+                         'body' => '{"first_name" : "Titi","last_name" : "Sow", "login" :"tsow","password" : "toto"}'
                         ]);
-    $this->assertEquals(200, $res->getStatusCode()); 
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
     $res = $client->get('http://localhost:8080/server.php/users/tsow', 
-                        ['headers' => ['Accept' => 'application/json']]);
+                        ['headers' => ['Accept' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'==']]);
     $data = json_decode($res->getBody(),TRUE);
-    $this->assertEquals(200, $res->getStatusCode()); 
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
     $this->assertEquals("Titi", $data["first_name"]);  
   }
 
+  private function rrmdir($dir){
+    foreach(glob($dir . '/*') as $file) {
+        if(is_dir($file))
+            $this->rrmdir($file);
+        else
+            unlink($file);
+    }
+    if($dir != "resources/users")
+      rmdir($dir);
+  }
 }
