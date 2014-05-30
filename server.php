3502e0d7a63c25acaf3d82f28dc5aec4799540d5
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-30 13:57:30 +0200

    Add middleware permission into server

--- server.php
+++ server.php
@@ -2,10 +2,12 @@
 
 require 'vendor/autoload.php';
 
+$ini = json_decode(file_get_contents('box.json'), TRUE);
+
 $app = new \Slim\Slim();
+$app->server = $ini;
 $app->add(new \CosyVerif\Server\Core());
+$app->add(new \CosyVerif\Server\Permission());
 $app->add(new \CosyVerif\Server\HttpBasicAuthentification());
-
-
 
 $app->run();
