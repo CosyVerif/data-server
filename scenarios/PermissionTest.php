23397cfc816b3f9d372e5413467dae1f2666e820
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-05-30 14:44:52 +0200

    add put user test into middleware permission

--- scenarios/PermissionTest.php
+++ scenarios/PermissionTest.php
@@ -48,4 +48,33 @@ class PermissionTest extends PHPUnit_Framework_TestCase
                          'exceptions' => false]);
     $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
   }
+
+  public function testPutUser()
+  {
+    Util::addUserRoot(RESOURCE_PRIVATE);
+    Util::addUser("Gael", "Thomas", "gthomas", "toto", USER_LIMIT, RESOURCE_PRIVATE);
+    Util::addUser("Toto", "Sow", "tsow", "toto", USER_LIMIT, RESOURCE_PUBLIC);
+    $client = new GuzzleHttp\Client();
+    //Administrator use get users
+    $encoded = base64_encode("root:toto");
+    $res = $client->put('http://localhost:8080/server.php/users/tsow', 
+                        ['headers' => ['Content-Type' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
+                         'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow","password" : "toto"}']);
+    $this->assertEquals(STATUS_OK, $res->getStatusCode()); 
+    //authentified user
+    $encoded = base64_encode("gthomas:toto");
+    $res = $client->put('http://localhost:8080/server.php/users/tsow', 
+                        ['headers' => ['Content-Type' => 'application/json', 
+                                       'Authorization' => 'Basic '.$encoded.'=='],
+                         'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow","password" : "toto"}',
+                         'exceptions' => false]);
+    $this->assertEquals(STATUS_FORBIDDEN, $res->getStatusCode()); 
+    //not authentified user
+    $res = $client->put('http://localhost:8080/server.php/users/tsow', 
+                        ['headers' => ['Content-Type' => 'application/json'],
+                         'body' => '{"first_name" : "Tata","last_name" : "Sow", "login" :"tsow","password" : "toto"}',
+                         'exceptions' => false]);
+    $this->assertEquals(STATUS_FORBIDDEN, $res->getStatusCode()); 
+  }
 }
