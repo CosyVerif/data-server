1fd68d6f71da98f4f039d8f05c1335c773d1c4e8
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-06-18 10:59:53 +0200

    modify middleware core for add the get resource list

--- src/CosyVerif/Server/Core.php
+++ src/CosyVerif/Server/Core.php
@@ -9,8 +9,116 @@ class Core  extends \Slim\Middleware
   {
     global $app;
 
-    $app->get('/(users|projects)/:id', function($id) use($app){
-      $data = $this->get($app->request->getResourceUri());
+    $app->group('/', function () use ($app) 
+    {
+        $app->group('users(/)', function () use ($app) 
+        {
+            $app->group('/:userID(/)', function () use ($app, $user) 
+            {
+                $app->group('/projects(/)', function () use ($app) 
+                {
+                    $app->get('', function() use($app){
+                      echo " :projects: ";
+                    })->setName("get-user-resourceList");
+                    $app->get('/:projectID+', function() use($app, $projectID){})->setName("user-project");
+                    $app->put('/:projectID+', function() use($app, $projectID){})->setName("user-project");
+                    $app->patch('/:projectID+', function() use($app, $projectID){})->setName("user-project");
+                    $app->delete('/:projectID+', function() use($app, $projectID){})->setName("user-project");
+                });
+                $app->group('/:others(/)', function () use ($app) 
+                {
+                    $app->group('/:other(/)', function () use ($app) 
+                    {
+                      $app->get('', function() use($app){
+                        echo " :other: ";
+                      })->setName("user-resource");
+                    });
+                    $app->get('', function() use($app){
+                      echo " :others: ";
+                    })->setName("user-resourceList");
+
+                });
+                $app->get('', function() use($app){
+                    $data = $this->get($app->request->getResourceUri(), true);
+                    if (!is_null($data)){$app->response->setBody($data);}
+                })->setName("user");
+                $app->put('', function() use($app){
+                    $data = json_decode($app->request->getBody(), TRUE);
+                    if (!is_array($data)){
+                      $app->halt(STATUS_UNPROCESSABLE_ENTITY);
+                    }
+                    $this->put($app->request->getResourceUri(), $data);
+                })->setName("user");
+                $app->patch('', function() use($app){
+                    
+                })->setName("user");
+                $app->delete('', function() use ($app){
+                  $this->delete($app->request->getResourceUri());
+                })->setName("user");
+            });
+          $app->get('', function() use($app){
+            $data = $this->get($app->request->getResourceUri(), true);
+            if (!is_null($data)){$app->response->setBody($data);}
+          })->setName("list");
+        });
+        $app->group('projects(/)', function () use ($app) 
+        {
+            $app->group('/:projectID(/)', function () use ($app) 
+            {
+                $app->group('/users(/)', function () use ($app) 
+                {
+                    $app->get('', function() use($app){
+                      echo " :users: ";
+                    })->setName("get-project-resourceList"); 
+                    $app->get('/:projectUserID(/)', function() use($app){})->setName("project-user"); 
+                    $app->put('/:projectUserID(/)', function() use($app){})->setName("project-user"); 
+                    $app->patch('/:projectUserID(/)', function() use($app){})->setName("project-user"); 
+                    $app->delete('/:projectUserID(/)', function() use($app){})->setName("project-user"); 
+                    $app->get('/:projectUserID/:other+', function() use($app){})->setName("project-user-resources"); 
+                    $app->put('/:projectUserID/:other+', function() use($app){})->setName("project-user-resource"); 
+                    $app->patch('/:projectUserID/:other+', function() use($app){})->setName("project-user-resource"); 
+                    $app->delete('/:projectUserID/:other+', function() use($app){})->setName("project-user-resource"); 
+
+                });
+                $app->group('/:others(/)', function () use ($app) 
+                {
+                    $app->group('/:otherID', function () use ($app) 
+                    {
+                        $app->get('', function() use($app){
+                          echo " :other: ";
+                        }); 
+                    });
+                    $app->get('', function() use($app){
+                      echo " :others: ";
+                    })->setName("project-resourceList");           
+                });
+                $app->get('', function() use($app){
+                  echo " :project: ";
+                })->setName("project");          
+            });
+            $app->get('', function() use($app){
+              $data = $this->get($app->request->getResourceUri(), true);
+              if (!is_null($data)){$app->response->setBody($data);}
+            })->setName("list");        
+        });
+        $app->get('', function() use($app){
+          $data = $this->get($app->request->getResourceUri(), true);
+          if (!is_null($data)){$app->response->setBody($data);}
+        })->setName("list");
+    });
+    /*
+    $app->get('/users/:id', function($id) use($app){
+      $data = $this->get($app->request->getResourceUri(), false);
+      if (!is_null($data)){$app->response->setBody($data);}
+    });
+
+    $app->get('/projects/:id', function($id) use($app){
+      $data = $this->get($app->request->getResourceUri(), false);
+      if (!is_null($data)){$app->response->setBody($data);}
+    });
+
+    $app->get('/(users|projects)', function() use($app){
+      $data = $this->get($app->request->getResourceUri(), true);
       if (!is_null($data)){$app->response->setBody($data);}
     });
 
@@ -25,13 +133,17 @@ class Core  extends \Slim\Middleware
     $app->delete('/(users|projects)/:id', function($id) use($app){
       $this->delete($app->request->getResourceUri());
     });
-
+*/
     $this->next->call();
   }
 
-  private function get($url)
+  private function get($url,$isList)
   { 
-    $resource = StreamJson::read($url);  
+    if ($isList){
+      $resource = StreamJson::readResourceList($url); 
+    } else {
+      $resource = StreamJson::readResource($url); 
+    }
     if (is_array($resource)){
       $resource = json_encode($resource);
     }
--- src/CosyVerif/Server/StreamJson.php
+++ src/CosyVerif/Server/StreamJson.php
@@ -5,7 +5,7 @@ require_once 'Constants.php';
 
 class StreamJson
 {
-  public static function read($url)
+  public static function readResource($url)
   {
     global $app;
     //Verify existing resource
@@ -27,6 +27,39 @@ class StreamJson
     return $resource;
   }
 
+  public static function readResourceList($url)
+  {
+    global $app;
+    //Verify existing resource list
+    if (!file_exists("resources".$url))
+    {
+      // Resource not found
+      $app->response->setStatus(STATUS_NOT_FOUND);
+      return null;
+    } 
+    else if(!file_exists("resources".$url."/info.json"))
+    {
+      $app->response->setStatus(STATUS_GONE);
+      return null;
+    }
+    $data = json_decode(file_get_contents("resources".$url."/info.json"), TRUE);
+    $resourceList = array();
+    foreach (glob("resources".$url.'/*') as $file) 
+    {
+      if (!is_dir($file))
+        continue;
+      else if (file_exists($file."/info.json"))
+      { 
+        $tmp = json_decode(file_get_contents($file."/info.json"), TRUE);
+        $resourceList[] = array('href' => $file, 'name' => $tmp["name"]);
+      }
+    }
+    $data["resource_list"] = $resourceList;
+    $app->response->setStatus(STATUS_OK);
+    $app->response->headers->set('Content-Type','application/json');
+    return $data;
+  }
+
   public static function write($url, $data)
   {
     global $app;
@@ -34,22 +67,56 @@ class StreamJson
     $is_ok = false;
     $info = "";
     $auth = "";
-    if (file_exists("resources".$url."/info.json")){
+    if (file_exists("resources".$url."/info.json"))
+    {
       // Resource not found
       $info = json_decode(file_get_contents("resources".$url."/info.json"), TRUE);
       //Update resource
-      foreach ($data["info"] as $field => $value){
+      foreach ($data["info"] as $field => $value)
+      {
         $info[$field] = $value;
       }
       $auth = json_decode(file_get_contents("resources".$url."/auth.json"), TRUE);
       //Update resource
-      foreach ($data["auth"] as $field => $value){
+      foreach ($data["auth"] as $field => $value)
+      {
         $info[$field] = $value;
       }
       $app->response->setStatus(STATUS_OK);
-    } else {
+    } 
+    else 
+    {
       if(!file_exists("resources".$url))
+      {
         mkdir("resources".$url);
+        $pattern = $app->router()->getCurrentRoute()->getPattern();
+        $regex = "#(^/users/:userID.?$|/projects/:project.?$)#";
+        if (preg_match($regex, $pattern))
+        {
+          $tmp = array();
+          mkdir("resources".$url."/formalisms");
+          $tmp["name"] = "Formalism list";
+          file_put_contents("resources".$url."/formalisms/info.json", json_encode($tmp));
+          mkdir("resources".$url."/models");
+          $tmp["name"] = "Model list";
+          file_put_contents("resources".$url."/models/info.json", json_encode($tmp));
+          mkdir("resources".$url."/scenarios");
+          $tmp["name"] = "scenarios list";
+          file_put_contents("resources".$url."/scenarios/info.json", json_encode($tmp));
+          mkdir("resources".$url."/services");
+          $tmp["name"] = "Service list";
+          file_put_contents("resources".$url."/services/info.json", json_encode($tmp));
+          mkdir("resources".$url."/executions");
+          $tmp["name"] = "Execution list";
+          file_put_contents("resources".$url."/executions/info.json", json_encode($tmp)); 
+          if (preg_match("#(^/users/:userID.?$)#", $pattern))
+          {
+            mkdir("resources".$url."/projects");
+            $tmp["name"] = "Project list";
+            file_put_contents("resources".$url."/projects/info.json", json_encode($tmp));
+          }
+        }
+      }
       $info = $data["info"];
       $auth = $data["auth"];
       $auth["password"] = password_hash($auth["login"].$auth["password"], PASSWORD_DEFAULT);
@@ -64,14 +131,29 @@ class StreamJson
   {
     global $app;  
     $is_ok = false;  
-    if (file_exists("resources".$url."/info.json")){
-      unlink("resources".$url."/info.json");
+    if (file_exists("resources".$url."/info.json"))
+    {
+      StreamJson::rrmdir("resources".$url,"resources".$url);
       $app->response->setStatus(STATUS_NO_CONTENT);
       $is_ok = true;
-    } else {
+    } 
+    else 
+    {
       $app->response->setStatus(STATUS_NOT_FOUND);
     }
     return is_ok;
+  }
+
+  public static function rrmdir($path, $newPath){
+    foreach(glob($newPath . '/*') as $file) 
+    {
+        if(is_dir($file))
+          StreamJson::rrmdir($path, $file);
+        else if ($file != $path."/auth.json")
+          unlink($file);      
+    }
+    if($newPath != $path)
+      rmdir($newPath);
   }
 }
 ?>
