a4178bf242deb98f157551e02db490b33facb3b2
Idrissa SOKHONA <rokysar@yahoo.fr>
2014-06-20 11:54:22 +0200

    Add user config service in to server

--- server.php
+++ server.php
@@ -2,10 +2,13 @@
 
 require 'vendor/autoload.php';
 
-$ini = json_decode(file_get_contents('box.json'), TRUE);
+$server_config_file = json_decode(file_get_contents('box.json'), TRUE);
+$user_config_file = parse_ini_file("config.ini");
+$user_config_file = array_map('strtolower', $user_config_file);
+$config = array_merge($server_config_file, $user_config_file);
 
 $app = new \Slim\Slim();
-$app->server = $ini;
+$app->config = $config;
 $app->add(new \CosyVerif\Server\Core());
 $app->add(new \CosyVerif\Server\Permission());
 $app->add(new \CosyVerif\Server\HttpBasicAuthentification());
