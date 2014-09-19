be00e5cfec81bebee90bc5f3b13e895f2572a16d
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-19 17:29:43 +0200

    Add parts for code coverage.

--- server.php
+++ server.php
@@ -9,6 +9,17 @@ $config = array_merge($server_config_file, $user_config_file);
 
 $app = new \Slim\Slim();
 $app->config = $config;
+
+if (array_key_exists("coverage", $config) && $config["coverage"]) {
+  $coverage = new PHP_CodeCoverage;
+  $coverage->start('server');
+  $app->get('/STOP', function () use ($app, $coverage) {
+    $coverage->stop();
+    $writer = new PHP_CodeCoverage_Report_HTML;
+    $writer->process($coverage, 'coverage');
+  });
+}
+
 \CosyVerif\Server\Routing\WebSiteMiddleware::register();
 \CosyVerif\Server\Routing\UserMiddleware::register();
 \CosyVerif\Server\Routing\ProjectMiddleware::register();
