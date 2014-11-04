613656bf3c15306be1e536d1023a03ce557a00db
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-11-04 17:10:27 +0100

    Current state...

--- src/cosy/server/resource/user.lua
+++ src/cosy/server/resource/user.lua
@@ -1,7 +1,6 @@
 local configuration = require "cosy.server.configuration"
-local resources     = require "cosy.server.resources"
+local Resources     = require "cosy.server.resources"
 local bcrypt        = require "bcrypt"
-local copas = require "copas.timer"
 
 local rounds = configuration.server.password_rounds
 
@@ -12,7 +11,10 @@ local function generate_key ()
   return result
 end
 
-local User = resources ()
+local User = {}
+Resources.user = User
+
+User.__index = User
 
 function User:__check (t)
   local errors = {}
@@ -23,43 +25,93 @@ function User:__check (t)
   return true
 end
 
-function User:new (t)
-  local resource = "${root}/users/${user}" % {
-    root = configuration.server.root,
-    user = t.username,
-  }
-  User [resource] = {
+local function create (t)
+  return setmetatable ({
     username       = t.username,
---    password       = bcrypt.digest (t.password, rounds),
+    password       = bcrypt.digest (t.password, rounds),
     fullname       = t.fullname,
     email          = t.email,
     validation_key = generate_key (),
     is_active      = false,
     is_public      = true,
-  }
---local haricot = require "haricot"
---local bs = haricot.new("localhost", 11300)
-  return User [resource]
+  }, User)
 end
 
+--local haricot = require "haricot"
+--local bs = haricot.new("localhost", 11300)
+
 -- Test:
-copas.addthread (function ()
-  local redis = configuration.redis.client
-  redis:flushall ()
-  print "Before add."
-  for i = 1, 1000 do
-    print (i)
-    User:new {
-      username = "user-${n}" % { n = i },
-      password = "toto",
-      fullname = "User ${n}" % { n = i },
-      email    = "user.${n}@gmail.com" % { n = i },
-    }
-    if i % 10 == 0 then
-      print (i)
+local copas = require "copas.timer"
+
+local nb_create =   50
+local nb_read   = 1000
+local nb_write  =  200
+
+print ("# create: " .. tostring (nb_create))
+print ("# write : " .. tostring (nb_write * nb_create))
+print ("# read  : " .. tostring (nb_read  * nb_create))
+
+local start_time = os.time ()
+local finished = nb_create * 3
+
+local function finish ()
+  local finish_time = os.time ()
+  local duration = finish_time - start_time
+  print ("Time: " .. tostring (duration) .. " seconds.")
+  local operations = nb_create + nb_write * nb_create + nb_read  * nb_create
+  print ("Average operations: " .. tostring (operations / duration) .. " per second.")
+  os.exit (0)
+end
+
+local function do_read (i)
+  for k = 1, nb_read do
+    local root = Resources {} [tostring (i)]
+    assert (root ~= nil)
+    for k, v in pairs (root) do
+      local _ = root.is_public
     end
   end
-  print "After add."
-end)
+  finished = finished - 1
+  if finished == 0 then
+    finish ()
+  end
+end
+
+local function do_write (i)
+  for k = 1, nb_write do
+    local root = Resources {} [tostring (i)]
+    for k, v in pairs (root) do
+      root.is_private = true
+    end
+  end
+  finished = finished - 1
+  if finished == 0 then
+    finish ()
+  end
+end
+
+local function do_create (i)
+  local root = Resources {}
+  root [tostring (i)] = create {
+    username = "user-${i}" % { i = i },
+    password = "toto",
+    fullname = "User ${i}" % { i = i },
+    email    = "user.${i}@gmail.com" % { i = i },
+  }
+  copas.addthread (function ()
+    do_read (i)
+  end)
+  copas.addthread (function ()
+    do_write (i)
+  end)
+  finished = finished - 1
+end
+
+for i = 1, nb_create do
+  copas.addthread (function ()
+    do_create (i)
+  end)
+end
+copas.loop ()
 
 return User
--- src/cosy/server/resources.lua
+++ src/cosy/server/resources.lua
@@ -1,23 +1,306 @@
 local configuration = require "cosy.server.configuration"
-                      require "cosy.server.redis"
                       require "cosy.util.string"
 local copas         = require "copas.timer"
-local json          = require "dkjson"
+local json          = require "cjson"
+local redis         = require "redis"
 
-local redis = configuration.redis.client
-local subscriber = configuration.redis.subscriber
-
-local DATA     = {}
 local CONTEXT  = {}
-
+local DATA     = {}
 local DELETED  = {}
 local DIRTY    = {}
 
+local function expand (t)
+  local result = {}
+  for k, v in pairs (t) do
+    result [#result + 1] = k
+    result [#result + 1] = v
+  end
+  return table.unpack (result)
+end
+
+local host    = configuration.redis.host
+local port    = configuration.redis.port
+local db      = configuration.redis.database
 local channel = "cosy-updates"
-local store   = setmetatable ({}, { __mode = "kv" })
+
+local Redis_mt = {
+  __mode  = "kv",
+  __copas = {},
+  __sync  = {},
+}
+
+function Redis_mt:__call (synchronous)
+  local id = coroutine.running ()
+  local container
+  if synchronous then
+    container = Redis_mt.__sync
+  else
+    container = Redis_mt.__copas
+  end
+  local found = container [id]
+  if found then
+    return found
+  end
+  local result = redis.connect ({
+    host      = host,
+    port      = port,
+    use_copas = not synchronous,
+  })
+  result:select (db)
+  container [id] = result
+  return result
+end
+
+local Redis = setmetatable ({}, Redis_mt)
+
+do
+  local client = Redis (true)
+  local root = configuration.server.root
+  client:hmset (root, expand {
+    _resource = root,
+    _type     = "root",
+  })
+end
+
+local Store_mt = {
+  __mode = "kv",
+}
+
+function Store_mt:__index (key)
+  local data = {
+    _resource = key,
+    [DIRTY]   = true,
+  }
+  self [key] = data
+  return data
+end
+
+local store   = setmetatable ({}, Store_mt)
+
+local Resources_mt = {
+  names   = {},
+  classes = {},
+}
+local Resources = setmetatable ({}, Resources_mt)
+
+local Resource_mt  = {}
+
+function Resources_mt:__index (key)
+  local _ = self
+  if type (key) == "string" then
+    return Resources_mt.classes [key]
+  elseif type (key) == "table" then
+    return Resources_mt.names [key]
+  else
+    assert (false)
+  end
+end
+
+function Resources_mt:__newindex (key, value)
+  local _ = self
+  assert (type (key) == "string")
+  assert (type (value) == "table")
+  Resources_mt.names [value] = key
+  Resources_mt.classes [key] = value
+end
+
+function Resources_mt:__call (t)
+  local _ = self
+  assert (type (t) == "table")
+  return setmetatable ({
+    [CONTEXT] = t,
+    [DATA   ] = store [configuration.server.root],
+  }, Resource_mt)
+end
+
+function Resource_mt:__index (key)
+  local in_mt = Resource_mt [key]
+  if in_mt then
+    return in_mt
+  end
+  local data = self [DATA]
+  if data [DIRTY] then
+    self:__refresh ()
+  end
+  local class    = Resources [data._type]
+  local in_class = class [key]
+  if in_class then
+    return in_class
+  end
+  local subdata = data [key]
+  if not subdata then
+    print "here"
+    assert (false)
+    return nil
+  elseif type (subdata) == "string" and subdata:find ("@=@", 1, true) == 1 then
+    local subresource = subdata:sub (4)
+    return setmetatable ({
+      [CONTEXT] = self [CONTEXT],
+      [DATA   ] = store [subresource],
+    }, Resource_mt)
+  elseif type (subdata) == "table" then
+    return setmetatable ({
+      [CONTEXT] = self [CONTEXT],
+      [DATA   ] = subdata,
+    }, Resource_mt)
+  else
+    return subdata
+  end
+end
+
+function Resource_mt:__create (resource, t)
+  assert (type (t) == "table")
+  assert (not t._resource)
+  t._resource = resource
+  t._type     = Resources [getmetatable (t)]
+  assert (t._type)
+  local parameters = {}
+  for k, v in pairs (t) do
+    local kt = type (k)
+    local vt = type (v)
+    if kt == "string" then
+      if vt == "string"  or vt == "boolean" or vt == "number"  then
+        parameters [k] = v
+      elseif vt == "table" then
+        if not v._resource then
+          local subresource = "${resource}/${key}" % {
+            resource = resource,
+            key      = k,
+          }
+          self:__create (subresource, v)
+        end
+        parameters [k] = "@=@${target}" % {
+          target = v._resource
+        }
+      end
+    end
+  end
+  store [resource] = t
+  local client = Redis ()
+  client:del     (resource)
+  client:hmset   (resource, expand (parameters))
+  client:publish (channel, json.encode {
+    origin   = configuration.server.uuid,
+    resource = resource,
+    action   = "create",
+  })
+  return resource
+end
+
+function Resource_mt:__newindex (key, value)
+  assert (type (key) ~= "table")
+  local data = self [DATA]
+  if data [DIRTY] then
+    self:__refresh ()
+  end
+  local class    = Resources [data._type]
+  assert (not class [key])
+  local id = tostring (coroutine.running ())
+  local resource = data._resource
+  data [key] = value
+  local vt = type (value)
+  local client = Redis ()
+  if vt == "string"  or vt == "boolean" or vt == "number"  then
+    client:hset (resource, key, value)
+  elseif vt == "table" then
+    if not value._resource then
+      local subresource = "${resource}/${key}" % {
+        resource = resource,
+        key      = key,
+      }
+      self:__create (subresource, value)
+    end
+    client:hset (resource, key, "@=@" .. tostring (value._resource))
+  else
+    assert (false)
+  end
+  client:publish (channel, json.encode {
+    origin   = configuration.server.uuid,
+    resource = resource,
+    action   = "update",
+  })
+end
+
+function Resource_mt:__eq (r)
+  local lhs = rawget (self, DATA)
+  local rhs = rawget (r   , DATA)
+  return lhs._resource == rhs._resource
+end
+
+function Resource_mt:__refresh ()
+  local data = self [DATA]
+  if not data [DIRTY] then
+    return
+  end
+  local key      = data._resource
+  local client   = Redis ()
+  local received = client:hgetall (key)
+  if pairs (received) (received) == nil then
+    data [DELETED] = true
+    assert (false)
+  end
+  print ("in " .. key)
+  for k in pairs (data) do
+    if type (k) == "string" and not received [k] then
+      print ("remove " .. k)
+      rawset (data, k, nil)
+    end
+  end
+  for k, v in pairs (received) do
+    print ("add " .. k .. " = " .. tostring (v))
+    if type (k) == "string" then
+      rawset (data, k, v)
+    end
+  end
+  data [DIRTY] = nil
+end
+
+function Resource_mt:__ipairs ()
+  local data = self [DATA]
+  self:__refresh ()
+  return coroutine.wrap (function ()
+    for k in ipairs (data) do
+      coroutine.yield (k, self [k])
+    end
+  end)
+end
+
+function Resource_mt:__pairs ()
+  local data = self [DATA]
+  self:__refresh ()
+  return coroutine.wrap (function ()
+    for k in pairs (data) do
+      if type (k) == "string" or type (k) == "boolean" or type (k) == "number" then
+        coroutine.yield (k, self [k])
+      end
+    end
+  end)
+end
+
+function Resource_mt:__mod (t)
+  assert (type (t) == "table")
+  local data     = self [DATA]
+  local resource = data._resource
+  self:__refresh ()
+  local parameters = {}
+  for k, v in pairs (t) do
+    if type (k) == "string" then
+      parameters [k] = v
+      data       [k] = v
+    end
+  end
+  local client = Redis ()
+  client:hmset   (resource, expand (parameters))
+  client:publish (channel, json.encode {
+    origin   = configuration.server.uuid,
+    resource = resource,
+    action   = "update",
+  })
+end
 
 copas.addthread (function ()
-  for message, _ in subscriber:pubsub { subscribe = { channel } } do
+  local client = Redis ()
+  for message, _ in client:pubsub { subscribe = { channel } } do
     if message.kind == "message" then
       local content = json.decode (message.payload)
       assert (content)
@@ -38,223 +321,7 @@ copas.addthread (function ()
   end
 end)
 
---[[
-  Resources.register [typename] = class
-  local r = Resources {
-    username = ...,
-    ...
-  }
-  local user = r.alinard
-  local model = user.philosophers
+Resources.root = {}
 
-  Permissions:
-  r.is_private = true
-  r.allow.x = "admin" | "read" | "write"
---]]
-
-local Resources_mt = {}
-local Resource_mt  = {}
-
-function Resources_mt:__call (t)
-  assert (type (t) == "table")
-  return setmetatable ({
-    [CONTEXT] = t,
-  }, Resource_mt)
-end
-
-function Resources_mt:__index (key)
-  local stored  = store [key]
-  if stored then
-    return stored
-  end
-  local exists = redis:exists (key)
-  if not exists then
-    return nil
-  end
-  stored = store [key] -- double check
-  if stored then
-    return stored
-  end
-  local data  = {
-    resource = key,
-    [DIRTY ] = true,
-  }
-  store [key] = data
-  return setmetatable ({
-    [CONTEXT] = self [CONTEXT],
-    [DATA   ] = data,
-  }, Resource_mt)
-end
-
-function Resources_mt:__newindex (key, value)
-  if value ~= nil or type (value) ~= table then
-    rawset (self, key, value)
-    return
-  end
-  if value == nil then
-    -- delete
-    store [key] = nil
-    redis:del (key)
-    redis:publish (channel, json.encode {
-      origin   = configuration.server.uuid,
-      resource = key,
-      action   = "delete",
-    })
-    return
-  end
-  if store [key] == value then
-    return
-  end
-  -- Check validity:
-  local data = {
-    resource = key,
-  }
-  for k, v in pairs (value) do
-    if type (k) == "string" then
-      rawset (data, k, v)
-      -- TODO: if v is table
-    end
-  end
-  local ok, errors = self:__check (data)
-  if not ok then
-    error (errors)
-  end
-  -- Update resource:
-  store [key] = data
-  local parameters = {}
-  for k, v in pairs (data) do
-    if type (k) == "string" then
-      parameters [#parameters + 1] = k
-      parameters [#parameters + 1] = v
-    end
-  end
-  redis:transaction (function (t)
-    t:del (key)
-    t:hmset (key, table.unpack (parameters))
-  end)
-  redis:publish (channel, json.encode {
-    origin   = configuration.server.uuid,
-    resource = key,
-    action   = "create",
-  })
-end
-
---
-
-function Resource_mt:__index (key)
-  local data   = rawget (self, DATA)
-  -- TODO: check access rights!
-  if data [DIRTY] then
-    self:__refresh ()
-  end
-  assert (not data [DELETED])
-  return data [key]
-end
-
-function Resource_mt:__newindex (key, value)
-  local data   = rawget (self, DATA)
-  if data [DIRTY] then
-    self:__refresh ()
-  end
-  -- TODO: check access rights!
-  self [key] = value
-  redis:hset (data.resource, key, value)
-  redis:publish (channel, json.encode {
-    origin   = configuration.server.uuid,
-    resource = key,
-    action   = "update",
-  })
-end
-
-function Resource_mt:__eq (r)
-  local lhs = rawget (self, DATA)
-  local rhs = rawget (r   , DATA)
-  return lhs.resource == rhs.resource
-end
-
-function Resource_mt:__refresh ()
-  local data     = rawget (self, DATA)
-  if not data [DIRTY] then
-    return
-  end
-  local key      = data.resource
-  local received = redis:hgetall (key)
-  if pairs (received) (received) == nil then
-    data [DELETED] = true
-    return
-  end
-  for k in pairs (data) do
-    if type (k) == "string" and not received [k] then
-      rawset (data, k, nil)
-    end
-  end
-  for k, v in pairs (received) do
-    if type (k) == "string" then
-      rawset (data, k, v)
-    end
-  end
-end
-
-function Resource_mt:__ipairs ()
-  local data = rawget (self, DATA)
-  self:__refresh ()
-  return coroutine.wrap (function ()
-    for k, v in ipairs (data) do
-      coroutine.yield (k, v)
-    end
-  end)
-end
-
-function Resource_mt:__pairs ()
-  local data = rawget (self, DATA)
-  self:__refresh ()
-  return coroutine.wrap (function ()
-    for k, v in pairs (data) do
-      coroutine.yield (k, v)
-    end
-  end)
-end
-
-function Resource_mt:__mod (t)
-  assert (type (t) == "table")
-  local data     = rawget (self, DATA)
-  local resource = data.resource
-  self:__refresh ()
-  local parameters = {}
-  for k, v in pairs (t) do
-    if type (k) == "string" then
-      parameters [#parameters + 1] = k
-      parameters [#parameters + 1] = v
-      data [k] = v
-    end
-  end
-  t:hmset (resource, table.unpack (parameters))
-  redis:publish (channel, json.encode {
-    origin   = configuration.server.uuid,
-    resource = resource,
-    action   = "update",
-  })
-end
-
-local resources = setmetatable ({}, Resources_mt)
-
-local Resources = {
-  classes = {}
-}
-
-function Resources:register (name, class)
-  assert (not Resources_mt.classes [name])
-  Resources_mt.classes [name] = class
-end
-
-function Resources:root (t)
-  local proxy = setmetatable ({
-    [CONTEXT] = t,
-  }, Resources_mt)
-  return proxy [configuration.server.root]
-  -- When loading:
-  -- * wrap the data within a proxy (context + dirty + ...)
-  -- * add functions contained in the class
-end
 
 return Resources
