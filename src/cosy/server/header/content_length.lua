03a149d9ad36bf99e03b09e72db6bcf3fdcc57bb
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-11-12 12:18:17 +0100

    Parse POST parameters.

--- src/cosy/server/header/content_length.lua
+++ src/cosy/server/header/content_length.lua
@@ -5,4 +5,14 @@ return function (context, length)
   local request = context.request
   length        = tonumber (length)
   request.body  = skt:receive (length):trim ()
+  -- Extract parameters:
+  if request.method == "POST" then
+    local parameters = request.parameters
+    for p in request.body:gmatch "([^;&]+)" do
+      local k, v = p:match "([^=]+)=(.*)"
+      k = url.unescape (k):gsub ("+", " ")
+      v = url.unescape (v):gsub ("+", " ")
+      parameters [k] = v
+    end
+  end
 end
